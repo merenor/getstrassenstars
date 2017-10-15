9b9560ad1ec9c1de07b34b7caa4044272555e3b3
Felix Werthschulte <felix@werthschulte.info>
2017-10-15 20:27:14 +0200

    second commit

--- getstrassenstars.py
+++ getstrassenstars.py
@@ -2,6 +2,7 @@
 
 import requests
 import os
+import time
 
 from bs4 import BeautifulSoup as Soup
 
@@ -11,7 +12,10 @@ TARGET = '/volume1/familie/videos/serien/strassenstars/'
 
 
 def log(text):
-    print(text)
+    current_time = time.strftime('%d.%m.%Y %H:%M:%S')
+    print(current_time, "-", text)
+    with open('getstrassenstars.log', 'w') as f:
+        f.write(current_time + ' - ' + text + '\n')
 
 
 def get_current_episode():
@@ -25,21 +29,44 @@ def get_current_episode():
         exit()
 
 
+def get_recorded_episodes():
+    with open('recorded_episodes.txt', 'r') as f:
+        return [line.strip() for line in f]
+
+
+def learn_episode(link):
+    lines = get_recorded_episodes()
+    lines.append(link)
+
+    with open('recorded_episodes.txt', 'a') as f:
+        for line in lines:
+            f.write(line + '\n')
+
+
 def download_video(source, targetdir):
+    """Tests if Episode is already downloaded.
+    Else, gets the actual video link and downloads the video to target folder."""
+
     if not targetdir.endswith('/'):
         targetdir += '/'
 
     log("Suche den Link zur Videodatei.")
 
     VIDEO = {'type': 'video/mp4'}
+
+    if source in get_recorded_episodes():
+        log("Aktuelle Episode bereits heruntergeladen.")
+        exit()
+    else:
+        learn_episode(source)
+
     link = Soup(requests.get(source).text, 'html.parser').find('source', attrs=VIDEO)
     if link is not None:
         filename = targetdir + link['src'].split('/')[-1]
         if not os.path.isfile(filename):
             log("Starte Download von " + link['src'] + " ...")
-            with open(filename, "wb") as file:
-                response = requests.get(link['src'])
-                file.write(response.content)
+            with open(filename, 'wb') as f:
+                f.write(requests.get(link['src']).content)
         else:
             log("Datei {} existiert bereits.".format(filename))
             exit()
